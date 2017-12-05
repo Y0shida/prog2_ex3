765873786a39aebcae2766961c4b825f86ff8cf4
Y0shida <e145753@ie.u-ryukyu.ac.jp>
2017-12-05 10:12:37 +0900

    ex3_3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,14 @@ import jp.ac.uryukyu.ie.pair02.*;
 public class Main{
     public static void main(String[] args){
         Dice dice = new Dice();
+        ExDice exdice = new ExDice(5,7);
         dice.play();
         System.out.println(dice.getValue());
+
+        for(int i = 0; i<10; i++){
+            exdice.play();
+            System.out.println("exdice"+i+"回目:"+ exdice.getValue());
+
+        }
     }
 }
